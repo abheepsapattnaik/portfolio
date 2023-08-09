1f5bd29b43f0b38ac21a24dc7c82ca57e5801bca
Abheepsa Pattnaik <abheepsapattnaik@gmail.com>
2023-08-09 17:01:09 +0530

    Abheepsa: About About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import AboutMe from "./component/AboutMe.jsx";
+import {BrowserRouter} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+          <div className="relative z-0">
+            <div className="bg-about ">
+                <div className="bg-cover bg-center bg-no-repeat">
+                    <AboutMe></AboutMe>
+                </div>
+
+            </div>
+          </div>
+      </BrowserRouter>
   );
 }
 
---
+++ src/component/AboutMe.jsx
@@ -0,0 +1,26 @@
+import {motion} from 'framer-motion';
+import {fadeIn, textVariant} from "../utils/motion.js";
+import {styles} from "../styles.js";
+
+const AboutMe = () => {
+    return (
+        <div className="mt-[10rem]">
+            <motion.div variants={textVariant()}>
+                <p className={styles.sectionSubText}>Introduction</p>
+                <h2 className={styles.sectionHeadText}>Briefly something about me</h2>
+            </motion.div>
+            <motion.p
+                variants={fadeIn('left', '', 0.1, 1)}
+                className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]">
+               Full Stack Developer. Team Leader . Web Development . Mobile Development . Tech Architecture
+                ...................
+                ................
+                .................
+                .................
+                .................
+            </motion.p>
+        </div>
+    );
+};
+
+export default AboutMe;
